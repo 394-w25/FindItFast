2e109e3d73af84d3192b241f8c7f5b05d9dd278a
Serena Lyou <serenalyou@serena.attlocal.net>
2025-01-22 09:01:55 -0600

    email verification

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -13,6 +13,10 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
+            if (!email.endsWith("@u.northwestern.edu")) {
+                setError("Please use a Northwestern email.");
+                return;
+            }
             await loginWithEmail(email, password); // Use Firebase email-password login
             navigate('/found'); // Redirect on successful login
         } catch (err) {
@@ -26,7 +30,13 @@ const Login = () => {
             const result = await signInWithGoogle();
             console.log('result:', result);
             console.log('result.user:', result.user);
-            const userID = (result.user).uid;
+            const userID = (result.user).uid;  
+            const email = (result.user).email; 
+
+            if (!email.endsWith("@u.northwestern.edu")) {
+                setError("Please use a Northwestern email.");
+                return;
+            }
 
             const userData = {
                 [userID]:
--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -14,8 +14,18 @@ const SignUp = () => {
 
     const handleSignUp = async (e) => {
         e.preventDefault();
+        if (username.length > 10) {
+            setError("Username must be less than 10 characters long.");
+        }
+        if (username.includes(' ')) {
+            setError("Username cannot contain spaces.");
+        }
+        if (!email.endsWith("@u.northwestern.edu")) {
+            setError("Please use a Northwestern email.");
+            return;
+        }
         if (password !== confirmPassword) {
-            setError('Passwords do not match');
+            setError('Passwords do not match.');
             return;
         }
         try {
@@ -35,6 +45,10 @@ const SignUp = () => {
 
             navigate('/found');
         } catch (err) {
+            if (err.code === "auth/email-already-in-use") {
+                setError("This email is already in use. Please sign in."); 
+                return;
+            }
             setError(err.message);
         }
     };
@@ -46,14 +60,10 @@ const SignUp = () => {
                 <input
                     type="text"
                     placeholder="Username"
+                    className="auth-input"
                     required
                     value={username}
-                    onChange={(e) => {
-                        const value = e.target.value;
-                        if (value.length <= 10 && !value.includes(' ')) {
-                            setUsername(value);
-                        }
-                    }}  
+                    onChange={(e) => setUsername(e.target.value)}  
                 />
                 <input
                     type="email"
